01ae6cd6594ff3ffc232b9df199ce264d98fd48e
Adam Skorek <askorek@gmail.com>
2022-12-22 17:51:19 +0100

    day1 done

--- day1.py
+++ day1.py
@@ -1,1 +1,13 @@
-print("hello world")
+with open("day1.txt") as f:
+    data = f.readlines()
+
+calories = [0]
+
+for line in data:
+    if line == "\n":
+        calories.append(0)
+    else:
+        calories[-1] += int(line.split('\n')[0])
+
+print(max(calories))
+print(sum(sorted(calories)[-3:]))
